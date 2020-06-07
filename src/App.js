000d2f310518dc68915199b624b7bc39a13d17f8
Michael <kllmmc23@gmail.com>
2020-06-07 03:44:09 -0600

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ const App = () => {
     <div className='App'>
       <header className="App-header">
         <Input placeholder='enter here' type='text'/>
+      
+      <h1 className='name title'>Names list</h1>
+      <NameTag firstName='Peter' lastName='Wilson'/>
+      <NameTag firstName='John' lastName='Smith'/>
+        <NameTag firstName='Jill' lastName='Bell' />
+        <NameTag></NameTag>
       </header>
       
     </div>
@@ -16,9 +22,3 @@ const App = () => {
 }
 
 export default App;
-
-// eslint-disable-next-line no-lone-blocks
-{/* <h1 className='name title'>Names list</h1>
-<NameTag firstName='Peter' lastName='Wilson'/>
-<NameTag firstName='John' lastName='Smith'/>
-<NameTag firstName='Jill' lastName='Bell'/> */}
--- src/Components/NameTags.jsx
+++ src/Components/NameTags.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const NameTag = (props) => {
+const NameTag = ({ firstName, lastName }) => {
+  if (!firstName && !lastName) {
     return (
       <div className="name">
-        <h3>First Name: {props.firstName}</h3>
-        <h3>Last Name: {props.lastName}</h3>
+        <h3>Invalid Name</h3>
       </div>
     );
-}
+  }
+
+  return (
+    <div className="name">
+      <h3>First Name: {firstName}</h3>
+      <h3>Last Name: {lastName}</h3>
+      {firstName === "John" && <div style={{ color: "green" }}>VIP</div>}
+    </div>
+  );
+};
 
 export default NameTag;
