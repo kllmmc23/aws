17d1993afd0ee7fd652ea96fd199a1fb609186c5
Michael <kllmmc23@gmail.com>
2020-06-07 02:23:22 -0600

    intro

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <div className="">hello World</div>
+            <ul>
+                <li>Hello</li>
+                <li>World</li>
+                <li>Hello</li>
+                <li>World</li>
+            </ul>
+        </div>
+    )
+}
+
+export default HelloWorld;
