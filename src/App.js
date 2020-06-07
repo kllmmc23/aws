534eee6128212e72e4052d298a4bb1afe6213e34
Michael <kllmmc23@gmail.com>
2020-06-07 12:56:25 -0600

    managing lists

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-lone-blocks */
 import React, { useState } from "react";
 import "./App.scss";
 import shortId from "shortid";
@@ -7,6 +8,7 @@ const initialNames = [
   { id: shortId.generate(), firstName: "John", lastName: "Johnson" },
   { id: shortId.generate(), firstName: "Peter", lastName: "Peterson" },
   { id: shortId.generate(), firstName: "Jill", lastName: "Jillson" },
+  { id: shortId.generate(), firstName: "Mary", lastName: "Kate" },
 ];
 console.log(initialNames);
 function App() {
@@ -15,25 +17,37 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Use State hooks</h1>
-        <NameTag
-          id={names[0].id}
-          firstName={names[0].firstName}
-          lastName={names[0].lastName}
-        />
-        <NameTag
-          id={names[1].id}
-          firstName={names[1].firstName}
-          lastName={names[1].lastName}
-        />
-        <NameTag
-          id={names[2].id}
-          firstName={names[2].firstName}
-          lastName={names[2].lastName}
-        />
+        <h1 className="name title">Name Lists</h1>
+        {names.map((v) => {
+          return (
+            <NameTag
+              key={v.id}
+              firstName={v.firstName}
+              lastName={v.lastName}
+            />
+          );
+        })}
       </header>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* <NameTag
+  id={names[0].id}
+  firstName={names[0].firstName}
+  lastName={names[0].lastName}
+/>
+<NameTag
+  id={names[1].id}
+  firstName={names[1].firstName}
+  lastName={names[1].lastName}
+/>
+<NameTag
+  id={names[2].id}
+  firstName={names[2].firstName}
+  lastName={names[2].lastName}
+/> */
+}
