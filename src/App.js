20a3f5400a4a8d1afac5337cc8128a7b12acb1f1
Michael <kllmmc23@gmail.com>
2020-06-07 12:49:07 -0600

    managing lists

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React, { useState } from "react";
 import "./App.scss";
+import shortId from "shortid";
+import NameTag from "./Components/NameTags";
 
+const initialNames = [
+  { id: shortId.generate(), firstName: "John", lastName: "Johnson" },
+  { id: shortId.generate(), firstName: "Peter", lastName: "Peterson" },
+  { id: shortId.generate(), firstName: "Jill", lastName: "Jillson" },
+];
+console.log(initialNames);
 function App() {
-  const [age, setAge] = useState(21);
-  const ageUpHandle = () => {
-    setAge(age + 1);
-    };
-    
-    const ageDownHandle = () => {
-      setAge(age - 1);
-    };
+  const [names, setNames] = useState(initialNames);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Use State hooks</h1>
-        <h2>Age: {age}</h2>
-        <button onClick={ageUpHandle}>Age up</button>
-        <button onClick={ageDownHandle}>Age down</button>
+        <NameTag
+          id={names[0].id}
+          firstName={names[0].firstName}
+          lastName={names[0].lastName}
+        />
+        <NameTag
+          id={names[1].id}
+          firstName={names[1].firstName}
+          lastName={names[1].lastName}
+        />
+        <NameTag
+          id={names[2].id}
+          firstName={names[2].firstName}
+          lastName={names[2].lastName}
+        />
       </header>
     </div>
   );
